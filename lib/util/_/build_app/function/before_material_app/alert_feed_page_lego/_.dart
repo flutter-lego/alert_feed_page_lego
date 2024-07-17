32fe7a64d630b5010acdd1c4f7ce7d8f8b0471ad
june lee <melodysdreamj@gmail.com>
2024-07-17 14:15:15 +0900

    Initial commit

---
+++ lib/util/_/build_app/function/before_material_app/alert_feed_page_lego/_.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../../main.dart';
+
+// @ReadyBeforeMaterialApp()
+// Future<void> readyForAlertFeedPageLego(BuildContext context) async {}
---
+++ lib/util/_/build_app/widget/run_app/alert_feed_page_lego/_.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import '../../../../../../main.dart';
+
+// @CoverRunApp()
+// Widget coverAlertFeedPageLego(Widget widget) {
+//   return widget;
+// }
